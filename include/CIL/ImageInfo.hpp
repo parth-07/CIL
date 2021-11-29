8d271cf2543bfc5c980bbdb484a9cbb7670aab43
Parth Arora <partharora99160808@gmail.com>
2021-11-29 22:47:10 +0530

    improve imageinfo class (#5)

--- include/CIL/ImageInfo.hpp
+++ include/CIL/ImageInfo.hpp
@@ -1,15 +1,20 @@
 #ifndef CIL_IMAGE_INFO_H
 #define CIL_IMAGE_INFO_H
+#include <CIL/ImageMatrix.hpp>
+#include <CIL/Pixel.hpp>
+
 #include <iostream>
 #include <setjmp.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string>
 
 #ifdef HAVE_CONFIG_H
 #include <config.h> // when build by autotools, this will be included
 #endif
 
 namespace CIL {
+
     enum class ColorModel
     {
         COLOR_RGB,
@@ -27,19 +32,42 @@ namespace CIL {
         PPM
     };
 
-    struct ImageInfo
+    // TODO: Make the design more extendible
+    class ImageInfo
     {
-        uint32_t width;
-        uint32_t height;
-        int num_components;
-        ColorModel color_model;
-        ImageType image_type;
-        uint8_t* data;
-        uint8_t sample_depth;
-        void print_image_info();
-        const void* internal_info = nullptr;
-        static void destroy(ImageInfo* img_info);
+        ColorModel m_color_model;
+        ImageType m_image_type;
+        ImageMatrix m_data;
+        const void* m_internal_info = nullptr;
+
+      public:
+        ImageInfo() {}
+        ImageInfo(uint32_t width, uint32_t height, uint32_t num_components,
+                  uint32_t sample_depth, ColorModel color_model,
+                  ImageType image_type, std::unique_ptr<uint8_t[]> data,
+                  const void* internal_info = nullptr);
+        ImageInfo(const ImageInfo&);
+        ImageInfo& operator=(const ImageInfo&);
+        ImageInfo(ImageInfo&&);
+        ImageInfo& operator=(ImageInfo&&);
+        ~ImageInfo();
+        uint32_t width() const;
+        uint32_t height() const;
+        uint32_t numComponents() const;
+        uint32_t sampleDepth() const;
+        uint64_t size() const;
+        const void* internalInfo() const;
+        ColorModel colorModel() const;
+        void setColorModel(ColorModel color_model);
+        bool save(const std::string& filename) const;
+        Pixel operator()(uint32_t row, uint32_t col);
+        uint8_t& operator()(uint32_t row, uint32_t col, int comp);
+        const uint8_t& operator()(uint32_t row, uint32_t col, int comp) const;
+        void printImageInfo();
+        bool empty() const;
     };
 
+    CIL::ImageInfo readImage(const char* filename);
+    CIL::ImageInfo readImage(const std::string& filename);
 } // namespace CIL
 #endif
--- lib/ImageInfo.cpp
+++ lib/ImageInfo.cpp
@@ -7,26 +7,139 @@
 #endif
 
 namespace CIL {
-    void ImageInfo::print_image_info()
+    ImageInfo::ImageInfo(uint32_t width, uint32_t height,
+                         uint32_t num_components, uint32_t sample_depth,
+                         ColorModel color_model, ImageType image_type,
+                         std::unique_ptr<uint8_t[]> data,
+                         const void* internal_info)
+        : m_color_model(color_model), m_image_type(image_type),
+          m_data(width, height, num_components, sample_depth, std::move(data)),
+          m_internal_info(internal_info)
+    {}
+
+    // TODO: We cannot just copy the internal_info pointer
+    ImageInfo::ImageInfo(const ImageInfo& other)
+        : m_color_model(other.m_color_model), m_image_type(other.m_image_type),
+          m_data(other.m_data), m_internal_info(other.m_internal_info)
+    {}
+
+    ImageInfo& ImageInfo::operator=(const ImageInfo& other)
     {
-        std ::cerr << "Width:\t" << width << "\nHeight:\t" << height
-                   << "\nNum Components:\t" << num_components
-                   << "\nColor space:\t" << static_cast<int>(color_model)
-                   << "\n";
+        if (this == &other)
+            return *this;
+        m_color_model = other.m_color_model;
+        m_image_type = other.m_image_type;
+        m_data = other.m_data;
+        return *this;
     }
-    void ImageInfo::destroy(ImageInfo* img_info)
+
+    // TODO: We cannot just copy the internal_info pointer
+    ImageInfo::ImageInfo(ImageInfo&& other)
+        : m_color_model(other.m_color_model), m_image_type(other.m_image_type),
+          m_data(std::move(other.m_data))
     {
-        switch (img_info->image_type)
+        auto temp = other.m_internal_info;
+        other.m_internal_info = m_internal_info;
+        m_internal_info = temp;
+    }
+
+    ImageInfo& ImageInfo::operator=(ImageInfo&& other)
+    {
+        if (this == &other)
+            return *this;
+        m_color_model = other.m_color_model;
+        m_image_type = other.m_image_type;
+        m_data = std::move(other.m_data);
+        return *this;
+    }
+
+    void ImageInfo::printImageInfo()
+    {
+        std ::cerr << "Width:\t" << m_data.width() << "\nHeight:\t"
+                   << m_data.height() << "\nNum Components:\t"
+                   << m_data.numComponents() << "\nColor space:\t"
+                   << static_cast<int>(m_color_model) << "\n";
+    }
+    ImageInfo::~ImageInfo()
+    {
+        switch (m_image_type)
         {
 #ifdef CIL_JPEG_ENABLED
-            case ImageType::JPEG: JPEG::JpegHandler::destroy(img_info); break;
+            case ImageType::JPEG: JPEG::JPEGHandler::destroy(this); break;
 #endif
 #ifdef CIL_PNG_ENABLED
-            case ImageType::PNG: PNG::destroyPNGImageInfo(img_info); break;
+            case ImageType::PNG: PNG::PNGHandler::destroy(this); break;
 #endif
             case ImageType::PPM: break;
         }
-        delete[] img_info->data;
-        delete img_info;
     }
+
+    uint32_t ImageInfo::width() const { return m_data.width(); }
+    uint32_t ImageInfo::height() const { return m_data.height(); }
+    uint32_t ImageInfo::numComponents() const { return m_data.numComponents(); }
+    uint32_t ImageInfo::sampleDepth() const { return m_data.sampleDepth(); }
+    ColorModel ImageInfo::colorModel() const { return m_color_model; }
+    uint64_t ImageInfo::size() const { return width() * height(); }
+
+    const void* ImageInfo::internalInfo() const { return m_internal_info; }
+    void ImageInfo::setColorModel(ColorModel color_model)
+    {
+        m_color_model = color_model;
+    }
+
+    Pixel ImageInfo::operator()(uint32_t row, uint32_t col)
+    {
+        return m_data(row, col);
+    }
+    uint8_t& ImageInfo::operator()(uint32_t row, uint32_t col, int comp)
+    {
+        return m_data(row, col, comp);
+    }
+
+    const uint8_t& ImageInfo::operator()(uint32_t row, uint32_t col,
+                                         int comp) const
+    {
+        return m_data(row, col, comp);
+    }
+
+    CIL::ImageInfo readImage(const char* filename)
+    {
+#ifdef CIL_PNG_ENABLED
+        if (PNG::PNGHandler::isPNGFile(filename))
+        {
+            return PNG::PNGHandler::readAs8RGB(filename);
+        }
+#endif
+#ifdef CIL_JPEG_ENABLED
+        if (JPEG::JPEGHandler::isJPEGFile(filename))
+        {
+            return JPEG::JPEGHandler::read(filename);
+        }
+#endif
+        return ImageInfo();
+    }
+
+    CIL::ImageInfo readImage(const std::string& filename)
+    {
+        return readImage(filename.c_str());
+    }
+
+    bool ImageInfo::save(const std::string& filename) const
+    {
+        switch (m_image_type)
+        {
+#ifdef CIL_PNG_ENABLED
+            case ImageType::PNG:
+                return PNG::PNGHandler::write(this, filename.c_str());
+#endif
+#ifdef CIL_JPEG_ENABLED
+            case ImageType::JPEG:
+                return JPEG::JPEGHandler::write(this, filename.c_str());
+#endif
+            case ImageType::PPM: break;
+        }
+        return false;
+    }
+
+    bool ImageInfo::empty() const { return m_data.empty(); }
 } // namespace CIL
